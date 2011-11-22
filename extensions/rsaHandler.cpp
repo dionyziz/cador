df98bafee743b48126b8d68680228ae0045b4029
Gregory Liras <gregliras@gmail.com>
2011-11-22 19:11:45 +0200

    new version of rsaHandler, 256bit asymetric encryption

--- extensions/rsaHandler.cpp
+++ extensions/rsaHandler.cpp
@@ -6,36 +6,46 @@
 
  * Creation Date : 21-11-2011
 
- * Last Modified : Mon 21 Nov 2011 03:54:24 PM EET
+ * Last Modified : Tue 22 Nov 2011 07:07:39 PM EET
 
  * Created By : Greg Liras <gregliras@gmail.com>
 
  _._._._._._._._._._._._._._._._._._._._._.*/
 #include "rsaHandler.h"
-
+using namespace std;
 rsaHandler::rsaHandler()
 {
 
-  int num = 8192;   //needs to be > 1024 to be considered secure;
-  int e = 65537;    //exponent
+  int num = 2048;   //needs to be > 1024 to be considered secure;
+  int e = 17;    //exponent
   //RSA_ADD(buf,num,entropy); //not needed since we have /dev/urandom
-  pub_key = RSA_generate_key(num,e,NULL,NULL);
-  if (pub_key == NULL)
-  {
-    Warning << "Failed to initialize pub_key";
-  }
+  //pub_key = RSA_generate_key(num,e,NULL,NULL);
+  //if (pub_key == NULL)
+  //{
+  //  cout << "Failed to initialize pub_key";
+  //}
   //RSA_ADD(buf,num,entropy); //not needed since we have /dev/urandom
+  priv_key = RSA_new();
   priv_key = RSA_generate_key(num,e,NULL,NULL);
   if (priv_key == NULL)
   {
-    Warning << "Failed to initialize priv_key";
+    cout << "Failed to initialize priv_key";
   }
+  pub_key = RSA_new();
+  get_public_RSA_key();
+
+  enc_data_length = 0;
+  orig_data_length = 0;
+
+  pub_remote_key = NULL;
+  encrypted = NULL;
+  decrypted = NULL;
+  pub_remote_key = NULL;
 }
 rsaHandler::~rsaHandler()
 {
   RSA_free(priv_key);
   RSA_free(pub_key);
-  RSA_free(pub_remote_key);
 }
 RSA *rsaHandler::get_public_key()
 {
@@ -43,31 +53,57 @@ RSA *rsaHandler::get_public_key()
 }
 void rsaHandler::set_public_remote_key(RSA *public_remote_key)
 {
+  cout << "OK got the key" << endl;
   this->pub_remote_key = public_remote_key;
 }
-std::string rsaHandler::encrypt_data(std::string data)
+string rsaHandler::encrypt_data(unsigned char * data)
 {
-  unsigned char *encrypted =  (unsigned char *)malloc(RSA_size(this->pub_remote_key));
-  this->rsa_padding = RSA_public_encrypt(data.size(), (unsigned char *) data.c_str(), encrypted, this->pub_remote_key, RSA_PKCS1_PADDING);
-  if (rsa_padding == -1)
+  this->orig_data_length = strlen((const char *) data);
+  if(this->encrypted != NULL)
   {
-    Warning << "RSA encryption failed"
+    clear_data_memory();
   }
-  sstream enc_data;
-  enc_data << *encrypted;
-  free(encrypted);
-  return enc_data.str();
+  this->encrypted =  (unsigned char *)malloc(RSA_size(this->pub_remote_key));
+  this->enc_data_length = RSA_public_encrypt(this->orig_data_length, (unsigned char *) data, this->encrypted, this->pub_remote_key, RSA_PKCS1_PADDING);
+  if (enc_data_length == -1)
+  {
+    cout << "RSA encryption failed" << endl;
+  }
+  return string((const char *)this->encrypted);
 }
-std::string rsaHandler::decrypr_data(std::string data)
+string rsaHandler::decrypt_data(unsigned char* data,long enc_data_length,long orig_data_length)
 {
-  unsigned char *decrypted =  (unsigned char *)malloc(this->rsa_padding);
-  int n = RSA_private_decrypt(this->enc_message_length, data.c_str(), decrypted, priv_key, RSA_PKCS1_PADDING);
+  this->orig_data_length = orig_data_length;
+  this->enc_data_length = enc_data_length;
+  if(this->decrypted != NULL)
+  {
+    clear_data_memory();
+  }
+  this->decrypted =  (unsigned char *)malloc(this->orig_data_length);
+  int n = RSA_private_decrypt(this->enc_data_length, data, this->decrypted, priv_key, RSA_PKCS1_PADDING);
   if(n == -1)
   {
-    Warning << "RSA decryption failed"
+    cout << "RSA decryption failed " << n << endl;
   }
-  sstream dec_data;
-  dec_data << *decrypted;
+  return string((const char *)this->decrypted);
+}
+long rsaHandler::get_enc_data_length()
+{
+  return this->enc_data_length;
+}
+long rsaHandler::get_orig_data_length()
+{
+  return this->orig_data_length;
+}
+void rsaHandler::get_public_RSA_key()
+{
+  BIO *bio_buffer = BIO_new(BIO_s_mem());
+  PEM_write_bio_RSAPublicKey(bio_buffer,priv_key);
+  PEM_read_bio_RSAPublicKey(bio_buffer,&pub_key,NULL,NULL);
+  BIO_free(bio_buffer);
+}
+void rsaHandler::clear_data_memory()
+{
+  free(encrypted);
   free(decrypted);
-  return dec_data.str();
 }
--- extensions/rsaHandler.h
+++ extensions/rsaHandler.h
@@ -6,17 +6,13 @@
 
 * Creation Date : 21-11-2011
 
-* Last Modified : Mon 21 Nov 2011 03:54:41 PM EET
+* Last Modified : Tue 22 Nov 2011 07:07:12 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
 _._._._._._._._._._._._._._._._._._._._._.*/
 #ifndef RSAHANDLER_H
 #define RSAHANDLER_H
-
-#include "../extension.h"
-#include "../conf.h"
-#include "../error.h"
 
 #include <cstdlib>
 #include <cstring>
@@ -27,23 +23,31 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <openssl/engine.h>
 #include <openssl/pem.h>
 #include <openssl/bio.h>
-#include <stream>
+//#include <stream>
 #include <sstream>
+#include <iostream>
 
 
-class rsaHandler() : public Cador_Extension
+class rsaHandler// : public Cador_Extension
 {
   private:
     RSA *pub_key;
     RSA *pub_remote_key;
     RSA *priv_key;
-    long enc_message_length;
+    long enc_data_length;
+    long orig_data_length;
+    void get_public_RSA_key();
+    unsigned char *encrypted;
+    unsigned char *decrypted;
   public:
     rsaHandler();
     ~rsaHandler();
     void set_public_remote_key(RSA *public_remote_key);
+    long get_enc_data_length();
+    long get_orig_data_length();
     RSA *get_public_key();
-    std::string encrypt_data(std::string data);
-    std::string decrypr_data(std::string data);
-}
+    std::string encrypt_data(unsigned char * data);
+    std::string decrypt_data(unsigned char * data,long enc_data_length,long data_length);
+    void clear_data_memory(); 
+};
 #endif //RSAHANDLER_H
